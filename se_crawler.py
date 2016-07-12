1c92ea09841dcc11bd1fe9d2e62b3c74550487bf
Joey <zhouyu5460@gmail.com>
2016-07-12 23:00:32 +0800

    added wait

--- se_crawler.py
+++ se_crawler.py
@@ -10,7 +10,8 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By 
-binary = FirefoxBinary('/home/xFrog/workspace/firefox/firefox')
+from selenium.webdriver.common.action_chains import ActionChains
+binary = FirefoxBinary('/opt/firefox/firefox')
 drv = webdriver.Firefox(firefox_binary=binary)
 link = "http://e1.englishtown.com/partner/englishcenters/cn"
 drv.get(link)
@@ -27,7 +28,17 @@ except TimeoutException:
     print "Loading took too much time!"
 html_list = drv.find_element_by_class_name("ue-menu-list")
 items = html_list.find_elements_by_tag_name("li")
+li_course = None
 for item in items:
-    text= item.text
-    print text
+    if item.text == "课程":
+        li_course = item 
+        break;
+        print li_course.text
+    #text= item.text
+#ActionChains(drv).move_to_element(li_course).move_to_element(drv.find_element(By.LINK_TEXT,"我的课程")).click().perform()
+ActionChains(drv).move_to_element(li_course).perform()
+WebDriverWait(drv, delay).until(EC.presence_of_element_located((By.LINK_TEXT, "我的课程")))
+my_course = drv.find_element(By.LINK_TEXT,"我的课程")
+ActionChains(drv).move_to_element(my_course).click().perform()
+    
 #print drv.page_source
