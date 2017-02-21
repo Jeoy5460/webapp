31293aed9e477df85ee1016ad49d2a8599b4beda
zhouyu <zhouyu5460@gmail.com>
2017-02-21 18:02:58 +0800

    add a url

--- ldoce_crawler/test.py
+++ ldoce_crawler/test.py
@@ -28,12 +28,24 @@ br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 
 # User-Agent (this is cheating, ok?)
 br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
-r = br.open('http://www.ldoceonline.com/browse/english/b/')
+r = br.open('http://www.ldoceonline.com/browse/english/a/')
 html = r.read()
 #print html
 #print br.response().read()
 soup = BeautifulSoup(html, "html5lib")
 innerDiv = soup.find(attrs={'class': "page_content"})
 links = innerDiv.find_all("a", href=re.compile("^http"))
+firstlink = None
 for link in links:
-    print link
+    if link.find("span"):
+        #print link
+        sublink = link['href']
+        r = br.open(sublink)
+        html = r.read()
+        soup = BeautifulSoup(html, "html5lib")
+        inner_ul = soup.find(attrs={'class': "browse_results"})
+        word_links = inner_ul.find_all("a", href=re.compile("^http"))
+        for link in word_links:
+            #text=link.find("span")
+            #print text.string.encode('utf-8').strip()
+            print link['href']
