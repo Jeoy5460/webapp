eded72df43e92af1514f6fcebbf688d99ff90bd3
zhouyu <zhouyu5460@gmail.com>
2017-03-09 17:50:03 +0800

    added trim to query script

--- ldoce_crawler/query_ldoce.sh
+++ ldoce_crawler/query_ldoce.sh
@@ -4,7 +4,8 @@ case $1 in
         egrep 'S1|S2|S3' core_vocabulary.lm
         ;;
     t)
-        awk  '{OFS= "\t"}{for(i=2;i<5;i++) $i="";print  $0}' $2 
+        #awk  '{OFS= "\t"}{for(i=2;i<5;i++) $i=""; print  $0}' $2 
+        awk  '{printf("%15-s %s %s %s %s %s %s\n",$1,$5,$6,$7,$8,$9,$10);}' $2 
         ;;
     *)
         ;;
